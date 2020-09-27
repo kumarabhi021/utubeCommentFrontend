c7be481d38ecb05990384b4b0ed15c4f95242745
Abhishek Kumar V <abhishek.v.kumar@ericsson.com>
2020-09-28 01:46:11 +0530

    subscribe UI added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useGoogleLogin } from "react-google-login";
@@ -9,9 +9,18 @@ const clientId =
 console.log(clientId);
 
 function App() {
+  let [showLogin, setShowLogin] = useState(true);
+  let [showSubscribe, setShowSubscribe] = useState(false);
+  let [url, setUrl] = useState("");
+  let videoId;
+  let [keywords, setKeywords] = useState([]);
+  let [showKeywords, setShowKeywords] = useState(false);
+  let tempKeyword;
   const onSuccess = (res) => {
     console.log(res.tokenId);
     console.log(res.profileObj.name);
+    setShowLogin(false);
+    setShowSubscribe(true);
   };
 
   const onFailure = (res) => {
@@ -27,12 +36,69 @@ function App() {
     scope: "https://www.googleapis.com/auth/youtube.readonly",
   });
 
+  let handleSubscribe = () => {
+    // handel the subscribe.
+    console.log(url);
+    videoId = url.split("v=")[1].substring(0, 11);
+    console.log(videoId);
+  };
+
+  let handleAddKeywords = () => {
+    console.log("keyword :", keywords);
+    console.log("tempkeyword :", tempKeyword);
+    setKeywords((keywords) => keywords.concat(tempKeyword));
+    setShowKeywords(true);
+    document.getElementById("keywordinput").value = "";
+  };
   return (
-    <div>
-      <button onClick={signIn} className="button">
-        <span className="buttonText">Sign in with Google</span>
-      </button>
-    </div>
+    <React.Fragment>
+      {showLogin && (
+        <div>
+          <button onClick={signIn} className="button">
+            <span className="buttonText">Sign in with Google</span>
+          </button>
+        </div>
+      )}
+      {showSubscribe && (
+        <div>
+          <label>
+            URL:
+            <input
+              type="text"
+              value={url}
+              onChange={(e) => {
+                setUrl(e.target.value);
+              }}
+              placeholder="enter the youtube livestream URL here"
+            />
+          </label>
+          {showKeywords && (
+            <div>comment Keywords entered :{keywords.toString()}</div>
+          )}
+          <br></br>
+          <label>
+            <input
+              id="keywordinput"
+              type="text"
+              value={tempKeyword}
+              onChange={(e) => {
+                tempKeyword = e.target.value;
+                console.log(tempKeyword);
+              }}
+              placeholder="enter the keywords here"
+            />
+          </label>
+          <button onClick={handleAddKeywords} className="button">
+            <span className="buttonText">Add keywords</span>
+          </button>
+          <br></br>
+
+          <button onClick={handleSubscribe} className="button">
+            <span className="buttonText">subscribe</span>
+          </button>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
 
